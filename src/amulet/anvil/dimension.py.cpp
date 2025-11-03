54b36e7c1d78a213a1df1a1b8a24860c157fc170
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-03 13:53:22 +0000

    Switch to smart_holder (#12)

--- src/amulet/anvil/dimension.py.cpp
+++ src/amulet/anvil/dimension.py.cpp
@@ -2,18 +2,17 @@
 #include <pybind11/stl.h>
 
 #include <amulet/pybind11_extensions/collections.hpp>
-#include <amulet/pybind11_extensions/nogil_holder.hpp>
 
 #include "dimension.hpp"
 
 namespace py = pybind11;
-namespace pyext = Amulet::pybind11_extensions;
 
 py::module init_anvil_dimension(py::module m_parent)
 {
     py::module m = m_parent.def_submodule("dimension");
 
-    py::class_<Amulet::AnvilDimensionLayer, pyext::nogil_shared_ptr<Amulet::AnvilDimensionLayer>> AnvilDimensionLayer(m, "AnvilDimensionLayer",
+    py::classh<Amulet::AnvilDimensionLayer>
+        AnvilDimensionLayer(m, "AnvilDimensionLayer", py::release_gil_before_calling_cpp_dtor(),
         "A class to manage a directory of region files.");
     AnvilDimensionLayer.def(
         py::init(
@@ -151,7 +150,8 @@ py::module init_anvil_dimension(py::module m_parent)
                 "If this is false, other calls will fail.\n"
                 "External Read:SharedReadWrite lock required."));
 
-    py::class_<Amulet::AnvilDimension, pyext::nogil_shared_ptr<Amulet::AnvilDimension>> AnvilDimension(m, "AnvilDimension",
+    py::classh<Amulet::AnvilDimension>
+        AnvilDimension(m, "AnvilDimension", py::release_gil_before_calling_cpp_dtor(),
         "A class to manage the data for a dimension.\n"
         "This can consist of multiple layers. Eg the region layer which contains chunk data and the entities layer which contains entities.");
     AnvilDimension.def(
